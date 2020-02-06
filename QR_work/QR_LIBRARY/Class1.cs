09330487faab06dcfe0e92bdfbbc5cc787c9fa3e
bryzgaalov <49521831+bryzgaalov@users.noreply.github.com>
2020-02-06 22:03:23 +0600

    Add files via upload

---
+++ QR_work/QR_LIBRARY/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace QR_LIBRARY
+{
+    public class QR
+    {
+         public static  void genQR(string text,int size)
+        {
+            string query = string.Format("code/?{0}&{1}&0", text, size);
+            var client = new RestClient("http://qrcoder.ru");
+
+            var request = new RestRequest(query);
+
+            var response = client.Execute(request);
+
+            var result = response.Content;
+        }
+    }
+}
